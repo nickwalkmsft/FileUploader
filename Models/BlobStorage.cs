fc9f24235f211e9f65d4cd5044bb280dfe40faa5
Nick Walker <nickwalk@microsoft.com>
2018-08-23 13:58:52 -0700

    Completed exercise

--- Models/BlobStorage.cs
+++ Models/BlobStorage.cs
@@ -1,8 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace FileUploader.Models
 {
@@ -17,26 +19,52 @@ namespace FileUploader.Models
 
         public Task Initialize()
         {
-            // Add Initialize code here
-            throw new NotImplementedException();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConfig.ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(storageConfig.FileContainerName);
+            return container.CreateIfNotExistsAsync();
         }
 
         public Task Save(Stream fileStream, string name)
         {
-            // Add Save code here
-            throw new NotImplementedException();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConfig.ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(storageConfig.FileContainerName);            
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(name);
+            return blockBlob.UploadFromStreamAsync(fileStream);
         }
 
-        public Task<IEnumerable<string>> GetNames()
+        public async Task<IEnumerable<string>> GetNames()
         {
-            // Add GetNames code here
-            throw new NotImplementedException();
+            List<string> names = new List<string>();
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConfig.ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(storageConfig.FileContainerName);
+
+            BlobContinuationToken continuationToken = null;
+            BlobResultSegment resultSegment = null;
+
+            do
+            {
+                resultSegment = await container.ListBlobsSegmentedAsync(continuationToken);
+
+                // Get the name of each blob.
+                names.AddRange(resultSegment.Results.OfType<CloudBlockBlob>().Select(b => b.Name));
+
+                continuationToken = resultSegment.ContinuationToken;
+            } while (continuationToken != null);
+
+            return names;
         }
 
         public Task<Stream> Load(string name)
         {
-            // Add Load code here
-            throw new NotImplementedException();
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConfig.ConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(storageConfig.FileContainerName);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(name);
+            return blockBlob.OpenReadAsync();
         }
     }
 }
